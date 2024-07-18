a3993b922b63f920474ce8a1b37bfaec73830729
bimo999 <nassim.ydkf@gmail.com>
2024-07-18 23:00:44 +0100

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 
 
-
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
